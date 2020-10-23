09e6430c24ef97da4a96995cb44e8e47f1c9c180
bit doer <bitdoer@gmail.com>
2020-10-23 01:09:23 -0500

    added class, total_order, todos for standard preds

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,6 @@ pub enum Class {
 }
 
 impl d32 {
-    
     /* ********************************************** *
      *                HELPER FUNCTIONS                *
      * ********************************************** */
@@ -52,13 +51,19 @@ impl d32 {
         // if exponent is of first form, then it is
         // G0 through G7; otherwise, it's G2 through
         // G9 (inclusive both times)
-        if self.exponent_form_one() {
+        if !self.is_finite() {
+            0
+        } else if self.exponent_form_one() {
             self.0 & 0x7f800000 >> 23
         } else {
             self.0 & 0x1fe00000 >> 21
         }
     }
-    
+
+    fn is_quiet(&self) -> bool {
+        self.is_nan() && !self.is_signaling()
+    }
+
     /* ********************************************** *
      *             GENERAL-COMP FUNCTIONS             *
      * ********************************************** */
@@ -96,7 +101,7 @@ impl d32 {
     }
 
     pub fn copy_sign(&self, y: &d32) -> Self {
-         d32(self.abs().0 | (y.0 & 0x80000000))
+        d32(self.abs().0 | (y.0 & 0x80000000))
     }
 
     pub fn encode_binary(&self) -> Self {
@@ -111,8 +116,46 @@ impl d32 {
      *               NON-COMP FUNCTIONS               *
      * ********************************************** */
 
+    pub fn is_754_version_1985() -> bool {
+        todo!()
+    }
+
+    pub fn is_754_version_2008() -> bool {
+        todo!()
+    }
+
+    pub fn is_754_version_2019() -> bool {
+        todo!()
+    }
+
+    pub fn class(&self) -> Class {
+        if self.is_signaling() {
+            Class::SignalingNaN
+        } else if self.is_nan() {
+            Class::QuietNaN
+        } else if self.is_sign_minus() {
+            if self.is_infinite() {
+                Class::NegativeInf
+            } else if self.is_normal() {
+                Class::NegativeNormal
+            } else if self.is_subnormal() {
+                Class::NegativeSubnormal
+            } else {
+                Class::NegativeZero
+            }
+        } else if self.is_infinite() {
+            Class::PositiveInf
+        } else if self.is_normal() {
+            Class::PositiveNormal
+        } else if self.is_subnormal() {
+            Class::PositiveSubnormal
+        } else {
+            Class::PositiveZero
+        }
+    }
+
     pub fn is_sign_minus(&self) -> bool {
-        self.0 | 0x80000000 == self.0
+        self.0 & 0x80000000 == 0x80000000
     }
 
     pub fn is_normal(&self) -> bool {
@@ -130,7 +173,7 @@ impl d32 {
         && (self.significand() == 0 || !self.is_canonical())
     }
 
-    pub fn is_subnormal(self) -> bool {
+    pub fn is_subnormal(&self) -> bool {
         self.is_finite()
             && !self.is_zero()
             && self.exponent() < 6
@@ -149,7 +192,7 @@ impl d32 {
         self.0 & 0x7e000000 == 0x7e000000
     }
 
-    pub fn is_canonical(self) -> bool {
+    pub fn is_canonical(&self) -> bool {
         (self.is_nan() && self.0 & 0x7df00000 == 0x7c000000)
             || (self.is_infinite() && self.0 & 0x7fffffff == 0x78000000)
             || (self.is_finite() && self.significand() <= 9999999)
@@ -160,7 +203,76 @@ impl d32 {
     }
 
     pub fn total_order(&self, y: &d32) -> bool {
-        todo!()
+        match (self.class(), y.class()) {
+            (Class::QuietNaN, Class::QuietNaN) => self.is_sign_minus() && !y.is_sign_minus(),
+            (Class::SignalingNaN, Class::SignalingNaN) => {
+                self.is_sign_minus() && !y.is_sign_minus()
+            }
+            (Class::QuietNaN, Class::SignalingNaN) => self.is_sign_minus(),
+            (Class::SignalingNaN, Class::QuietNaN) => !y.is_sign_minus(),
+            (Class::QuietNaN, _) => self.is_sign_minus(),
+            (Class::SignalingNaN, _) => self.is_sign_minus(),
+            (_, Class::QuietNaN) => !y.is_sign_minus(),
+            (_, Class::SignalingNaN) => !y.is_sign_minus(),
+            (Class::NegativeInf, Class::NegativeInf) => true,
+            (Class::NegativeInf, _) => true,
+            (_, Class::NegativeInf) => false,
+            (Class::PositiveInf, Class::PositiveInf) => true,
+            (Class::PositiveInf, _) => false,
+            (_, Class::PositiveInf) => true,
+            (Class::NegativeNormal, Class::NegativeNormal) => {
+                (self.significand() > y.significand() && self.exponent() >= y.exponent())
+                    || (self.exponent() - y.exponent() > 6)
+                    || (self.exponent() >= y.exponent()
+                        && (self.significand() as u64 * 10u64.pow(self.exponent() - y.exponent())
+                            >= y.significand() as u64))
+                    || (self.exponent() <= y.exponent()
+                        && (self.significand() as u64
+                            > y.significand() as u64 * 10u64.pow(y.exponent() - self.exponent())))
+            }
+            (Class::NegativeNormal, _) => true,
+            (_, Class::NegativeNormal) => false,
+            (Class::NegativeSubnormal, Class::NegativeSubnormal) => {
+                (self.significand() > y.significand() && self.exponent() >= y.exponent())
+                    || (self.exponent() - y.exponent() > 6)
+                    || (self.exponent() >= y.exponent()
+                        && (self.significand() as u64 * 10u64.pow(self.exponent() - y.exponent())
+                            >= y.significand() as u64))
+                    || (self.exponent() < y.exponent()
+                        && (self.significand() as u64
+                            > y.significand() as u64 * 10u64.pow(y.exponent() - self.exponent())))
+            }
+            (Class::NegativeSubnormal, _) => true,
+            (_, Class::NegativeSubnormal) => false,
+            (Class::NegativeZero, Class::NegativeZero) => self.exponent() >= y.exponent(),
+            (Class::NegativeZero, _) => true,
+            (_, Class::NegativeZero) => false,
+            (Class::PositiveZero, Class::PositiveZero) => self.exponent() <= y.exponent(),
+            (Class::PositiveZero, _) => true,
+            (_, Class::PositiveZero) => false,
+            (Class::PositiveSubnormal, Class::PositiveSubnormal) => {
+                (self.significand() < y.significand() && self.exponent() <= y.exponent())
+                    || (y.exponent() - self.exponent() > 6)
+                    || (self.exponent() <= y.exponent()
+                        && (self.significand() as u64
+                            <= y.significand() as u64 * 10u64.pow(y.exponent() - self.exponent())))
+                    || (self.exponent() > y.exponent()
+                        && (self.significand() as u64 * 10u64.pow(self.exponent() - y.exponent())
+                            < y.significand() as u64))
+            }
+            (Class::PositiveSubnormal, _) => true,
+            (_, Class::PositiveSubnormal) => false,
+            (Class::PositiveNormal, Class::PositiveNormal) => {
+                (self.significand() < y.significand() && self.exponent() <= y.exponent())
+                    || (y.exponent() - self.exponent() > 6)
+                    || (self.exponent() <= y.exponent()
+                        && (self.significand() as u64
+                            <= y.significand() as u64 * 10u64.pow(y.exponent() - self.exponent())))
+                    || (self.exponent() > y.exponent()
+                        && (self.significand() as u64 * 10u64.pow(self.exponent() - y.exponent())
+                            < y.significand() as u64))
+            }
+        }
     }
 
     pub fn total_order_mag(&self, y: &d32) -> bool {
@@ -169,8 +281,8 @@ impl d32 {
 
     pub fn same_quantum(&self, y: &d32) -> bool {
         (self.is_nan() && y.is_nan())
-        || (self.is_infinite() && y.is_infinite())
-        || (self.is_finite() && y.is_finite() && self.exponent() == y.exponent())
+            || (self.is_infinite() && y.is_infinite())
+            || (self.is_finite() && y.is_finite() && self.exponent() == y.exponent())
     }
 }
 
